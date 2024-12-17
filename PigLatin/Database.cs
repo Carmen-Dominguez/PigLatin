bf31bbfce6b67c9ae2e70e0ff1612035366bee24
Carmen Dominguez <carmengdominguez94@gmail.com>
2024-12-17 17:01:42 +0200

    feat: project to remote

---
+++ PigLatin/Database.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigLatin
+{
+    class Database
+    {
+        OdbcCommand cmd;
+        OdbcConnection conn;
+        OdbcDataReader reader;
+
+        public void ConnectDB()
+        {
+            try
+            {
+                conn = new OdbcConnection("Dsn=SimpleDSN");
+                conn.Open();
+                Console.WriteLine("Connected to SimpleDB");
+            }
+            catch (OdbcException oe)
+            {
+                Console.WriteLine(oe.Message, "Database Connection Error");
+            }
+        }
+
+        public void DisconnectDB()
+        {
+            conn.Close();
+        }
+
+        private bool ExecuteCommand(string query)
+        {
+            try
+            {
+                ConnectDB();
+                cmd = conn.CreateCommand();
+                cmd.CommandText = query;
+                cmd.ExecuteReader();
+                return true;
+            }
+            catch (OdbcException ex)
+            {
+                Console.WriteLine(ex.Message, "Database Execute Command Error");
+                return false;
+            }
+            finally
+            {
+                DisconnectDB();
+            }
+        }
+
+        public OdbcDataReader ExecuteQuery(String query)
+        {
+            try
+            {
+                ConnectDB();
+                cmd = conn.CreateCommand();
+                cmd.CommandText = query;
+                return cmd.ExecuteReader();
+            }
+            catch (OdbcException ex)
+            {
+                Console.WriteLine(ex.Message, "Database Execute Query Error");
+                return null;
+            }
+        }
+
+        //Add a new user to the database
+        public bool AddTelUser(string chatID, string name, string pass)
+        {
+            bool ans = false;
+            reader = ExecuteQuery("SELECT [chatID] FROM [telUser] WHERE [chatID] = '" + chatID + "'");
+
+            if (!(reader.HasRows))
+            {
+                reader.Close();
+                ans = ExecuteCommand("INSERT INTO [telUser] VALUES ('" + chatID + "', '" + name + "', '"+ pass +"')");
+                return ans;
+            }
+            else
+                return ans;
+        }
+
+        //Add user to task
+        public bool AddToTask(string taskID, string chatID)
+        {
+            bool ans = false;
+            reader = ExecuteQuery("SELECT [chatID] FROM [crTable] WHERE [taskID] = '" + taskID + "' AND [chatID] = '" + chatID + "';");
+
+            if (!(reader.HasRows))
+            {
+                reader.Close();
+                ans = ExecuteCommand("INSERT INTO [crTable] VALUES('" + taskID + "', '" + chatID + "')");
+                
+                return ans;
+            }
+            else
+                return ans;
+        }
+
+        //Get task members
+        public List<string> GetMemberIDs(string taskID)
+        {
+            List<string> ans = new List<string>(); 
+            reader = ExecuteQuery("SELECT [chatID] FROM [crTable] WHERE [taskID] = '" + taskID + "'");
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    ans.Add(reader.GetString(0));
+                }
+                return ans;
+            }
+            else
+                return null;
+        }
+
+        //Check Password
+        public bool CheckPass(string chatID, string pass)
+        {
+            bool ans = false;
+            reader = ExecuteQuery("SELECT [chatID] FROM [telUser] WHERE [chatID] = '" + chatID + "' AND [pass]= '"+ pass +"'");
+            if (reader.HasRows)
+            {
+                ans = true;
+            }
+            return ans;
+        }
+        
+        //Get taskID's
+        public List<string> GetTaskIDs()
+        {
+            List<string> ans = new List<string>();
+            reader = ExecuteQuery("SELECT [taskID] FROM [task]");
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    ans.Add(reader.GetString(0));
+                }
+                return ans;
+            }
+            else
+                return null;
+        }
+
+        //Get the task description
+        public OdbcDataReader GetTask(string taskID)
+        {
+            reader = ExecuteQuery("SELECT [tDesc] FROM [task] WHERE [taskID] = '" + taskID + "'");
+
+            if (reader.HasRows)
+                return reader;
+            else
+                return null;
+        }
+    }
+}
---
+++ PigLatin/Program.cs
@@ -0,0 +1,144 @@
+﻿/*Author:   Carmen Dominguez
+ Created:   11 December 2017*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+
+namespace PigLatin
+{
+    class Program
+    {
+        //Variables
+        static int x = 1;
+        static IDictionary<int, string> dict = new Dictionary<int, string>();
+        static Database db = new Database();
+
+        //The Bot
+        private static TelegramBotClient Bot = new TelegramBotClient("474984703:AAFXDgNWP7X89YPF9JWb3aHJTjgZ_wmI8pA");
+
+        public void InitPic()
+        {
+            dict.Add(1, "Hello Sky");
+            dict.Add(2, "Hello Island");
+            dict.Add(3, "Hello Sea");
+            dict.Add(4, "Hello Ocean");
+            dict.Add(5, "Hello World");
+            dict.Add(6, "Hello Pale Blue Dot");
+            dict.Add(7, "Hello Galaxy");
+            dict.Add(8, "Hello Galaxy Cluster");
+            dict.Add(9, "Hello Universe");
+            dict.Add(10, "Hello Multi Universe");
+            dict.Add(11, "Ran Out of Existance");
+        }
+
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+            if (message == null || message.Type != MessageType.TextMessage) return;
+
+            //If message contains something
+            db.ConnectDB();
+            String chatID = message.Chat.Id.ToString();
+            String name = message.Chat.FirstName + " " + message.Chat.LastName; /*Get the username from Telegram*/
+            String pass = "";
+
+            if (message.Text.StartsWith("/start"))
+            {
+                if (db.AddTelUser(chatID, name, pass))
+                {
+                    Console.WriteLine(chatID + " has been added");
+                    await Bot.SendTextMessageAsync(message.Chat.Id, "You have been added to SimpleBot.");
+                }
+                else
+                {
+                    Console.WriteLine(chatID + " couldn't be added");
+                    await Bot.SendTextMessageAsync(message.Chat.Id, "Something went wrong. You may already be subscribed.");
+                }
+            }
+            else
+
+            if (message.Text.StartsWith("/text"))
+            {
+                await Bot.SendTextMessageAsync(message.Chat.Id, "Message was received");
+            }
+            else
+
+                if (message.Text.StartsWith("/photo zoom out"))
+            {
+                await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.UploadPhoto);
+                string b = dict[x++];
+                string file = @"C:\\Users\cdomi\source\repos\PigLatin\PigLatin\img\" + b + ".jpg";
+
+                var fileName = file.Split('\\').Last();
+
+                using (var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    var fts = new FileToSend(fileName, fileStream);
+
+                    await Bot.SendPhotoAsync(message.Chat.Id, fts, b);
+                }
+            }
+
+            else
+                //Add user to task and/or return task details
+                foreach (string taskID in db.GetTaskIDs())
+                {
+                    if (message.Text.StartsWith("/" + taskID))
+                    {
+                        if (db.CheckPass(name, pass)) /*Check password*/
+                        {
+                            if (db.AddToTask(taskID, chatID))
+                            {
+                                Console.WriteLine(chatID + " has been added to " + taskID);
+                                await Bot.SendTextMessageAsync(message.Chat.Id, "You have been added to The " + taskID + " Group.");
+                            }
+                            await Bot.SendTextMessageAsync(message.Chat.Id, db.GetTask(taskID).GetString(0));
+                        }
+                    }
+                }
+        }
+
+        //send Tasks to members
+        public void Task(string mess)
+        {
+            foreach (string taskID in db.GetTaskIDs())
+            {
+                if (mess == taskID)
+                {
+                    string task = db.GetTask(taskID).GetString(0);
+                    foreach (string chatID in db.GetMemberIDs(taskID))
+                    {
+                        Bot.SendTextMessageAsync(chatID, task);
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program bla = new Program();
+            bla.InitPic();
+            Bot.OnMessage += BotOnMessageReceived;
+
+            ChatId carmen = "177678603";
+
+            var me = Bot.GetMeAsync().Result;
+
+            Console.Title = me.Username;
+            Bot.StartReceiving();
+            while (true)
+            {
+                String mess = "" + Console.ReadLine();
+                bla.Task(mess);
+                Bot.SendTextMessageAsync(carmen, mess);
+            }
+        }
+    }
+}
+
